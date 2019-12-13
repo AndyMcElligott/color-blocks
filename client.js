be0312d0de6b9033a386de56f41a30fc0048ffde
AndyMcElligott <andymc2315@gmail.com>
2019-12-13 09:04:48 -0600

    completed

--- client.js
+++ client.js
@@ -1,30 +1,29 @@
+/* eslint-disable no-use-before-define */
 $(document).ready(readyNow);
 
-function readyNow(){
-    $(`#redButton`).on('click', addRed)
-    $(`#yellowButton`).on('click', addYellow)
-    $(`#greenButton`).on('click', addGreen)
-    $(`#blueButton`).on('click', addBlue)
+function readyNow() {
+  $(`.redButton`).on('click', addRed);
+  $(`.yellowButton`).on('click', addYellow);
+  $(`.greenButton`).on('click', addGreen);
+  $(`.blueButton`).on('click', addBlue);
 }
 
-function addRed(){
-    let button = $('<button id="redButton">red</button>');
-    $('.container').append(button);
+function addRed() {
+  console.log('in addRed function');
+  $('.placeForBox').append(`<div class="redLeader"></div>`);
 }
 
-function addYellow(){
-    let button = $('<button id="yellowButton">red</button>');
-    $('.container').append(button);
-}    
+function addYellow() {
+  console.log('in addYellow function');
+  $('#yellowNum').append(`<button id="yellowFive">red</button>`);
+}
 
-function addBlue(){
-    let button = $('<button id="blueButton">red</button>');
-    $('.container').append(button);
-}    
-    
-function addGreen(){
-    let button = $('<button id="greenButton">red</button>');
-    $('.container').append(button);
-}        
+function addBlue() {
+  console.log('in addBlue function');
+  $('#blueNum').append(`<div class="blueBaron"></div>`);
+}
 
-let ul = $('<ul id=" ')
+function addGreen() {
+  console.log('in addGreen function');
+  $('#greenNum').append(`<div class="greenGoblin"></div>`);
+}
